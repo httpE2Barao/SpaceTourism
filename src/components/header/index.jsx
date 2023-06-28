19995fbe4afe78a60661ed7074cb7c6f3ca0e009
httpE2Barao <elias.barao@hotmail.com>
2023-06-28 19:43:58 -0300

    media queries pag inicial

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,31 +4,31 @@ const Header = () => {
   return (
     <header className='relative flex items-center py-5 px-6 md:pl-10 justify-between'>
       {/* Logo */}
-      <img src="./images/logo.svg" alt="Space Tourism Logo" className='cursor-pointer' />
+      <img src="./images/logo.svg" alt="Space Tourism Logo" className='cursor-pointer md:pt-5' />
       {/* Header Line */}
-      <div className='hidden w-px h-0.5 bg-cinza/30'></div>
+      <div className='hidden w-2/3 h-0.5 bg-cinza/30'></div>
       {/* Nav Bar Desktop */}
       <input className='hidden' type="checkbox" name="menu" id="menu" />
       <label htmlFor="menu">
-        <img src="./images/menu.svg" alt="Menu" className='w-8 h-8 cursor-pointer md:hidden' />
+        <img src="./images/menu.svg" alt="Menu" className='w-8 h-8 cursor-pointer md:hidden '/>
       </label>
       <ul className='
         hidden
-        absolute top-0 right-0
-        h-screen w-3/5 md:h-auto
+        absolute top-0 right-0 md:top-5
+        h-screen md:h-24 
         md:flex flex-col md:flex-row gap-7 md:gap-10 
         bg-cinza/10 backdrop-blur-md
         text-branco 
-        pl-10 py-8 md:pr-20 
+        pl-10 md:pr-20 
         '>
         <img src="./images/menu-fechar.svg" alt="Fechar" className='md:hidden w-6 h-6 mr-5 self-end cursor-pointer'/>
-        <li className='cursor-pointer uppercase pr-100 hover:border-r-4 md:hover:border-r-0 md:hover:border-b-4'>
+        <li className='cursor-pointer uppercase pr-100 py-8 hover:border-r-4 md:hover:border-r-0 md:hover:border-b-4'>
           <b className='mr-2'>00</b> Home</li>
-        <li className='cursor-pointer uppercase pr-100 hover:border-r-4 md:hover:border-r-0 md:hover:border-b-4'>
+        <li className='cursor-pointer uppercase pr-100 py-8 hover:border-r-4 md:hover:border-r-0 md:hover:border-b-4'>
           <b className='mr-2'>01</b> Destino</li>
-        <li className='cursor-pointer uppercase pr-100 hover:border-r-4 md:hover:border-r-0 md:hover:border-b-4'>
+        <li className='cursor-pointer uppercase pr-100 py-8 hover:border-r-4 md:hover:border-r-0 md:hover:border-b-4'>
           <b className='mr-2'>02</b> Tripulação</li>
-        <li className='cursor-pointer uppercase pr-100 hover:border-r-4 md:hover:border-r-0 md:hover:border-b-4'>
+        <li className='cursor-pointer uppercase pr-100 py-8 hover:border-r-4 md:hover:border-r-0 md:hover:border-b-4'>
           <b className='mr-2'>03</b> Tecnologia</li>
       </ul>
     </header>
--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -6,13 +6,17 @@ const Home = () => {
     <>
       <article className="home">
         <Header />
-        <section className="flex flex-col h-5/6 text-center px-2 py-4 pt-6 gap-6">
-          <h2 className="text-text font-secondary uppercase text-sm">
-            Então, você quer viajar para o</h2>
-          <h1 className="text-branco font-main uppercase text-6xl">
-            Espaço</h1>
-          <p className="w-4/5 mx-auto text-text font-secondary text-sm tracking-widest">
-            Vamos encarar; se você quer ir ao espaço, é melhor realmente ir para o espaço sideral e não pairar na borda. Prepare-se, sente e relaxe, porque vamos lhe dar uma experiência verdadeiramente fora deste mundo!</p>
+        <section className="flex flex-col h-5/6 text-center px-2 pt-7
+          md:flex-row md:items-center 
+        ">
+          <div className="flex flex-col gap-4 md:w-1/2 md:text-left md:pl-20">
+            <h2 className="text-text font-secondary uppercase text-sm lg:text-base md:pl-10">
+              Então, você quer viajar para o</h2>
+            <h1 className="text-branco font-main uppercase text-6xl md:text-7xl lg:text-9xl md:pl-10">
+              Espaço</h1>
+            <p className="mx-auto px-4 md:pl-10 text-text font-secondary text-xs md:text-sm lg:text-base tracking-widest">
+              Vamos encarar; se você quer ir ao espaço, é melhor realmente ir para o espaço sideral e não pairar na borda. Prepare-se, sente e relaxe, porque vamos lhe dar uma experiência verdadeiramente fora deste mundo!</p>
+          </div>
 
           <div className="relative m-auto">
             <button className="peer bg-branco w-40 h-40 rounded-full uppercase font-main text-xl tracking-widest ">
