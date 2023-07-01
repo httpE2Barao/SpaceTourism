ff27a3afea6408ad976ed6380200f99f282bdfe9
httpE2Barao <elias.barao@hotmail.com>
2023-06-30 21:05:13 -0300

    media querie para desktop tela: Tripulantes

--- src/pages/tripulacao/index.jsx
+++ src/pages/tripulacao/index.jsx
@@ -36,6 +36,7 @@ const Tripulacao = () => {
       <section className="font-secondary text-branco
        flex flex-col items-center p-4 pb-10 gap-8 
        md:gap-2 md:pb-0
+       lg:items-start
        ">
         <div className="order-0 tracking-widest md:mr-auto md:mt-8 md:ml-8">
           <h2 className="uppercase text-xl md:mr-auto md:mb-8">
@@ -45,9 +46,13 @@ const Tripulacao = () => {
         </div>
 
         <img src={`./images/${imageName}`} alt="Foto dele(a)" 
-        className="order-1 md:order-5 w-10/12 md:w-96 md:mt-6" />
+        className="foto_tripulante order-1 lg:self-end md:order-5 w-10/12 
+        md:w-96 md:mt-6 lg:absolute lg:bottom-0 lg:right-40
+        "/>
 
-        <ul className="order-3 md:order-4 flex items-center gap-8">
+        <ul className="order-3 md:order-4 flex items-center gap-8 
+        lg:ml-20 lg:absolute lg:bottom-10
+        ">
           {tripulacao.map((pessoa, index) => (
             <li
               key={index}
@@ -60,49 +65,57 @@ const Tripulacao = () => {
           ))}
         </ul>
         <h3 className="order-2 font-main text-cinza 
-          font-semibold uppercase text-lg">
+          font-semibold uppercase text-lg lg:ml-20 lg:mt-10">
           {currentOne}
         </h3>
         {currentOne === 'Comandante' &&
           <>
-            <h2 className="order-3 font-main uppercase text-3xl md:text-5xl">
+            <h2 className="order-3 font-main uppercase text-3xl md:text-5xl
+            lg:ml-20
+            ">
               Douglas Hurley
             </h2>
             <p className="order-3 pb-4 text-lg md:w-9/12 
-              text-center md:text-xl leading-8">
+              text-center lg:text-left md:text-xl leading-8 lg:w-1/3 lg:ml-20">
               Douglas Gerald Hurley é um engenheiro americano, ex-piloto do Corpo de Fuzileiros Navais e ex-astronauta da NASA. Ele foi ao espaço pela terceira vez como comandante da missão Demo-2 da Crew Dragon.
             </p>
           </>
         }
         {currentOne === 'Organizador' &&
           <>
-            <h2 className="order-3 font-main uppercase text-3xl md:text-5xl">
+            <h2 className="order-3 font-main uppercase text-3xl md:text-5xl
+            lg:ml-20
+            ">
               Mark Shuttleworth
             </h2>
-            <p className="order-3 pb-4 text-lg md:w-9/12  
-              text-center md:text-xl leading-8">
+            <p className="order-3 pb-4 text-lg md:w-9/12 
+              text-center lg:text-left md:text-xl leading-8 lg:w-1/3 lg:ml-20">
               Mark Richard Shuttleworth é o fundador e CEO da Canonical, a empresa responsável pelo sistema operacional Ubuntu, baseado em Linux. Ele conquistou o feito de se tornar o primeiro sul-africano a viajar para o espaço como turista espacial.
             </p>
           </>
         }
         {currentOne === 'Piloto' &&
           <>
-            <h2 className="order-3 font-main uppercase text-3xl md:text-5xl">
+            <h2 className="order-3 font-main uppercase text-3xl md:text-5xl
+            lg:ml-20
+            ">
               Victor Glover
             </h2>
-            <p className="order-3 pb-4 text-lg md:w-9/12  
-              text-center md:text-xl leading-8">
+            <p className="order-3 pb-4 text-lg md:w-9/12 
+              text-center lg:text-left md:text-xl leading-8 lg:w-1/3 lg:ml-20">
               Victor Glover foi piloto no primeiro voo operacional da SpaceX Crew Dragon para a Estação Espacial Internacional. Além disso, Glover é comandante na Marinha dos Estados Unidos, onde pilota um F/A-18. Ele fez parte da tripulação da Expedição 64 e desempenhou o papel de engenheiro de sistemas de voo da estação.
             </p>
           </>
         }
         {currentOne === 'Engenheira' &&
           <>
-            <h2 className="order-3 font-main uppercase text-3xl md:text-5xl">
+            <h2 className="order-3 font-main uppercase text-3xl md:text-5xl
+            lg:ml-20
+            ">
               Anousheh Ansari
             </h2>
-            <p className="order-3 pb-4 text-lg md:w-9/12  
-              text-center md:text-xl leading-8">
+            <p className="order-3 pb-4 text-lg md:w-9/12 
+              text-center lg:text-left md:text-xl leading-8 lg:w-1/3 lg:ml-20">
               Anousheh Ansari é uma engenheira iraniana-americana e co-fundadora da Prodea Systems. Ela foi a quarta turista espacial a financiar sua própria viagem, sendo a primeira mulher a fazê-lo para a Estação Espacial Internacional (ISS) e a primeira iraniana no espaço.
             </p>
           </>
